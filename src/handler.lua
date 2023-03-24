24932703002848cc1837dbce3e39369cc15cb85c
姬常峰 <jichf@qq.com>
2023-03-24 16:17:28 +0800

    decrease priority

--- src/handler.lua
+++ src/handler.lua
@@ -2,7 +2,7 @@ local access = require "kong.plugins.http-proxy.access"
 
 local HTTPProxyHandler = {
   VERSION  = "0.1.0-1",
-  PRIORITY = 990,
+  PRIORITY = 10,
 }
 
 function HTTPProxyHandler:access(conf)
